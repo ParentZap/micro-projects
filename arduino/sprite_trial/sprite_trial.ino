b260ac09057935f44d3d00ff96670fabfd567aac
Zach Parent <zap122333@gmail.com>
2021-12-05 21:49:35 -0500

    Work on getting dirty_rects set up. Vectors are currently broken

--- arduino/sprite_trial/sprite_trial.ino
+++ arduino/sprite_trial/sprite_trial.ino
@@ -1,8 +1,12 @@
+#include <cstdlib>
 #include "Adafruit_Arcada.h"
+#include "ArduinoSTL.h"
 
 Adafruit_Arcada arcada;
 uint16_t x;
 uint16_t y;
+//std::vector<std::tuple<uint16_t, uint16_t, uint16_t, uint16_t>> dirty_rects;
+//std::vector<int> my_vec;
 
 void setup() {
   Serial.begin(115200);
@@ -13,7 +17,7 @@ void setup() {
   y = 0;
 }
 
-void loop() {
+void update() {
   arcada.readButtons();
   uint8_t buttons = arcada.justPressedButtons();
   int16_t joystick_x = arcada.readJoystickX();
@@ -23,9 +27,25 @@ void loop() {
   int8_t dy = (int8_t) ((float)joystick_y/512.0*5.0);
   x += dx;
   y += dy;
+}
 
+void clean_dirty_rects() {
+//  for (int i = 0; i < dirty_rects.size(); i++) {
+//    std::tuple<uint16_t, uint16_t, uint16_t, uint16_t> dirty_rect = dirty_rects[i];
+//    arcada.display->fillRect(dirty_rect[0], dirty_rect[1], dirty_rect[2], dirty_rect[3], ARCADA_BLACK);
+//  }
+//  dirty_rects.clear();
+}
+
+void draw() {
   Serial.printf("X: %u, Y: %u\n", x, y);
+  arcada.display->fillRect(x, y, 20, 20, ARCADA_WHITE);
+}
+
+void loop() {
+  clean_dirty_rects();
+  update();
+  draw();
   
-  arcada.display->drawRect(x, y, 20, 20, ARCADA_WHITE);
   delay(50);
 }
